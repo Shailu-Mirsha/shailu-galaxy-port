bd9de27805ac32cb791f2c8b58537b7eb42411fe
Shailu Mirsha <shailumirhsa.cs24@stellamaryscoe.edu.in>
2025-10-21 12:44:26 +0530

    Your message

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,16 @@
 import React from "react";
 import Navigation from "./components/Navigation";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Contact from "./components/Contact";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-
-const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <>
       <Navigation />
-      <main>
-        <Hero />
-        <About />
-        <Projects />
-        <Skills />
-        <Contact />
-      </main>
       <Toaster />
       <Sonner />
       <TooltipProvider>
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -16,7 +16,6 @@ const Index = () => {
       <About />
       <Projects />
       <Skills />
-      <Resume />
       <Contact />
     </div>
   );
